6cc678c2f3a94f004935065634cc2ead7adeaf71
Carl Hirschman <carlhi@microsoft.com>
2021-12-05 14:10:06 -0800

    Solve Dec 5 part two.

--- AdventOfCode2021/Dec5.cs
+++ AdventOfCode2021/Dec5.cs
@@ -52,43 +52,88 @@ namespace AdventOfCode2021
                 }
             }
 
-           /* for (int y = 0; y < grid.GetLength(0); y++)
+            int numDangerous = 0;
+            for (int y = 0; y < grid.GetLength(0); y++)
             {
                 for (int x = 0; x < grid.GetLength(1); x++)
                 {
-                    var current = new Point(x, y);
-
-                    // Count # of lines touching this point.
-                    int count = 0;
-                    foreach (Line line in lines)
+                    if (grid[y, x] >= 2)
                     {
-                        if (line.IsHorizontal())
-                        {
-                            int minX = Math.Min(line.Begin.X, line.End.X);
-                            int maxX = Math.Max(line.Begin.X, line.End.X);
-                            if (line.Begin.Y == current.Y &&
-                                minX <= current.X && current.X <= maxX)
-                            {
-                                count++;
-                            }
-                        }
-                        else
-                        {
-                            int minY = Math.Min(line.Begin.Y, line.End.Y);
-                            int maxY = Math.Max(line.Begin.Y, line.End.Y);
-
-                            if (line.Begin.X == current.X && 
-                                minY <= current.Y && current.Y <= maxY)
-                            {
-                                count++;
-                            }
-                        }
+                        numDangerous++;
                     }
 
-                    grid[y, x] = count;
+                    if (show)
+                    {
+                        string output = grid[y, x] > 0 ? grid[y, x].ToString() : ".";
+                        Console.Write(output);
+                    }
                 }
-            }*/
 
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Num dangerous = {0}.", numDangerous);
+        }
+
+        public static void Solve_Part_Two(bool show = false)
+        {
+            List<Line> lines = PuzzleInputReader.GetPuzzleLines(@"c:\docs\adventofcode2021\dec5.txt")
+                                    .Select(l => new Line(l))
+                                    .ToList();
+
+            int bottomX = lines.Select(l => l.Begin.X).Max();
+            int temp = lines.Select(l => l.End.X).Max();
+            if (temp > bottomX)
+            {
+                bottomX = temp;
+            }
+
+            int bottomY = lines.Select(l => l.Begin.Y).Max();
+            temp = lines.Select(l => l.End.Y).Max();
+            if (temp > bottomY)
+            {
+                bottomY = temp;
+            }
+
+            var grid = new int[bottomY + 1, bottomX + 1];
+            foreach (Line line in lines)
+            {
+                if (line.IsHorizontal())
+                {
+                    int minX = Math.Min(line.Begin.X, line.End.X);
+                    int maxX = Math.Max(line.Begin.X, line.End.X);
+                    for (int x = minX; x <= maxX; x++)
+                    {
+                        grid[line.Begin.Y, x]++;
+                    }
+                }
+                else if (line.IsVertical())
+                {
+                    int minY = Math.Min(line.Begin.Y, line.End.Y);
+                    int maxY = Math.Max(line.Begin.Y, line.End.Y);
+                    for (int y = minY; y <= maxY; y++)
+                    {
+                        grid[y, line.Begin.X]++;
+                    }
+                }
+                else
+                {
+                    int xDir = (line.Begin.X > line.End.X) ? -1 : 1;
+                    int yDir = (line.Begin.Y > line.End.Y) ? -1 : 1;
+
+                    int currX = line.Begin.X - xDir;
+                    int currY = line.Begin.Y - yDir;
+                    do
+                    {
+                        currX += xDir;
+                        currY += yDir;
+                        grid[currY, currX]++;
+
+                    } while (currX != line.End.X && currY != line.End.Y);
+                }
+            }
+
+            
             int numDangerous = 0;
             for (int y = 0; y < grid.GetLength(0); y++)
             {
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            Dec5.Solve_Part_One(show: false);
+            Dec5.Solve_Part_Two(show: false);
         }
     }
 }

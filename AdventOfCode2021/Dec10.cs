789cb1c3083c9a7e0fbc133af53552c478d50eb7
Carl Hirschman <carlhi@microsoft.com>
2021-12-10 13:14:18 -0800

    Solve Dec 10 Part Two.

--- AdventOfCode2021/Dec10.cs
+++ AdventOfCode2021/Dec10.cs
@@ -35,6 +35,74 @@ namespace AdventOfCode2021
             Console.WriteLine("Total score = {0}.", score);
         }
 
+        public static void Solve_Part_Two(bool show = false)
+        {
+            var completionScores = new List<long>();
+            foreach (string line in PuzzleInputReader.GetPuzzleLines(@"c:\docs\adventofcode2021\dec10.txt"))
+            {
+                if (show)
+                {
+                    Console.Write("{0} ", line);
+                }
+
+                bool correct = true;
+                var stack = new Stack<char>();
+                foreach (char ch in line)
+                {
+                    if (IsChunkBegin(ch))
+                    {
+                        stack.Push(ch);
+                    }
+                    else
+                    {
+                        char endCh = stack.Pop();
+                        char expectedCh = GetExpectedEndChar(endCh);
+                        if (expectedCh != ch)
+                        {
+                            if (show)
+                            {
+                                Console.WriteLine("Expected {0}, but found {1} instead.", expectedCh, ch);
+                            }
+
+                            correct = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (correct)
+                {
+                    long score = 0;
+                    while (stack.Count > 0)
+                    {
+                        char next = stack.Pop();
+                        char endCh = GetExpectedEndChar(next);
+
+                        if (show)
+                        {
+                            Console.Write(endCh);
+                        }
+
+                        score = (5 * score) + GetCompletionScore(endCh);
+                    }
+
+                    if (show)
+                    {
+                        Console.WriteLine(" - {0} total points.", score);
+                    }
+
+                    completionScores.Add(score);
+                }
+            }
+
+            completionScores.Sort();
+
+            int index = completionScores.Count / 2;
+            Console.WriteLine("{0} scores, middle score will be at {1}.", completionScores.Count, index);
+
+            Console.WriteLine("Score = {0}.", completionScores[index]);
+        }
+
         private static char GetExpectedEndChar(char ch)
         {
             ;
@@ -51,6 +119,27 @@ namespace AdventOfCode2021
 
                 case '<':
                     return '>';
+
+                default:
+                    throw new ArgumentException($"Unexpected character {ch}.");
+            }
+        }
+
+        private static int GetCompletionScore(char ch)
+        {
+            switch (ch)
+            {
+                case ')':
+                    return 1;
+
+                case ']':
+                    return 2;
+
+                case '}':
+                    return 3;
+
+                case '>':
+                    return 4;
 
                 default:
                     throw new ArgumentException($"Unexpected character {ch}.");
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            Dec10.Solve_Part_One();
+            Dec10.Solve_Part_Two(show: false);
         }
     }
 }
